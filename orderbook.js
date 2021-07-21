0c17f2a76b84d095ff4c01734cb29f981e2b579b
Kevin Salina <kevin.salina25@gmail.com>
2021-07-21 14:18:13 -0400

    Added comments to codebase

--- orderbook.js
+++ orderbook.js
@@ -1,7 +1,10 @@
 const orderBook = (existingBook, incomingOrder) => {
+  // Check if exisitingBook has any values
   if (existingBook.length <= 0) return [incomingOrder]
 
+  // Loop through exisiting book using a for..of loop. Also extracting iteration indexes
   for (const [index, currentOrder] of existingBook.entries()) {
+    // Put special criteria into own variable, to clean up code
     // eslint-disable-next-line max-len
     const misMatchCriteria = ((incomingOrder.type === 'sell' && (incomingOrder.price <= currentOrder.price)) || (incomingOrder.type === 'buy' && incomingOrder.price >= currentOrder.price))
     const matchingPriceCriteria = currentOrder.price === incomingOrder.price
@@ -19,6 +22,7 @@ const orderBook = (existingBook, incomingOrder) => {
       } else if (incomingOrder.quantity > currentOrder.quantity && matchingPriceCriteria) {
         incomingOrder.quantity -= currentOrder.quantity
         existingBook.splice(index, 1)
+        // If there still are existing objects that match incoming order, use recursion to call function again
         if (existingBook.some(order => order.type !== incomingOrder.type && order.price === incomingOrder.price)) {
           return orderBook(existingBook, incomingOrder)
         }
