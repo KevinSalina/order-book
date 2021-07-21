1dd5654b650611ac2579479539d6a884aaf4761c
Kevin Salina <kevin.salina25@gmail.com>
2021-07-21 13:36:21 -0400

    Removed the need to create an empty updatedBook array. Now just returning an updated version of the exisitingBook array

--- orderbook.js
+++ orderbook.js
@@ -1,9 +1,5 @@
 const orderBook = (existingBook, incomingOrder) => {
-  let updatedBook = []
-
-  if (existingBook.length <= 0) {
-    return existingBook.concat(incomingOrder)
-  }
+  if (existingBook.length <= 0) return [incomingOrder]
 
   for (const [index, currentOrder] of existingBook.entries()) {
     // eslint-disable-next-line max-len
@@ -13,33 +9,28 @@ const orderBook = (existingBook, incomingOrder) => {
     if (currentOrder.type !== incomingOrder.type && misMatchCriteria) {
       if (currentOrder.quantity === incomingOrder.quantity) {
         existingBook.splice(index, 1)
-        updatedBook = existingBook
 
-        return updatedBook
+        return existingBook
       } else if (currentOrder.quantity > incomingOrder.quantity && matchingPriceCriteria) {
         currentOrder.quantity -= incomingOrder.quantity
         existingBook.push(...existingBook.splice(index, 1))
-        updatedBook = existingBook
 
-        return updatedBook
+        return existingBook
       } else if (incomingOrder.quantity > currentOrder.quantity && matchingPriceCriteria) {
         incomingOrder.quantity -= currentOrder.quantity
         existingBook.splice(index, 1)
         if (existingBook.some(order => order.type !== incomingOrder.type && order.price === incomingOrder.price)) {
-          let updatedOrder = incomingOrder
-
-          incomingOrder = []
-
-          return orderBook(existingBook, updatedOrder)
+          return orderBook(existingBook, incomingOrder)
         }
-        updatedBook = existingBook.concat(incomingOrder)
+
+        return existingBook.concat(incomingOrder)
       }
     } else {
-      updatedBook = existingBook.concat(incomingOrder)
+      return existingBook.concat(incomingOrder)
     }
   }
 
-  return updatedBook
+  return existingBook
 }
 
 module.exports = orderBook
